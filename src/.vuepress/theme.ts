f7793652b88feb45fcd82c497507563f5e8681c0
Zhenghao Zhang <zhangzhenghao@hotmail.com>
2022-11-08 08:48:16 +0800

    update hostname

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -3,7 +3,7 @@ import { enNavbar, zhNavbar } from "./navbar/index.js";
 import { enSidebar, zhSidebar } from "./sidebar/index.js";
 
 export default hopeTheme({
-  hostname: "https://vuepress-theme-hope-docs-demo.netlify.app",
+  hostname: "https://gorse.io",
 
   author: {
     name: "zhenghaoz",
