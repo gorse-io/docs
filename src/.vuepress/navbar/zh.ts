5b353cbd9320b28ee4fd2daf3af7ed31595eb6ac
canomh <canomh@163.com>
2022-11-12 20:49:36 +0800

    Update zh.ts

--- src/.vuepress/navbar/zh.ts
+++ src/.vuepress/navbar/zh.ts
@@ -1,30 +1,35 @@
 import { navbar } from "vuepress-theme-hope";
 
 export const zhNavbar = navbar([
-  "/zh/",
-  { text: "案例", icon: "discover", link: "/zh/demo/" },
+  "/",
+  { text: "文档", icon: "explore", link: "/docs/" },
+  { text: "博客", icon: "article-fill", link: "/blog/" },
   {
-    text: "指南",
-    icon: "creative",
-    prefix: "/zh/guide/",
+    text: "源码",
+    icon: "github-fill",
     children: [
       {
-        text: "Bar",
-        icon: "creative",
-        prefix: "bar/",
-        children: ["baz", { text: "...", icon: "more", link: "" }],
+        text: "核心",
+        children: [
+          { text: "Gorse", icon: "code", link: "https://github.com/gorse-io/gorse" },
+        ],
       },
       {
-        text: "Foo",
-        icon: "config",
-        prefix: "foo/",
-        children: ["ray", { text: "...", icon: "more", link: "" }],
+        text: "示例",
+        children: [
+          { text: "GitRec", icon: "preview", link: "https://github.com/gorse-io/gitrec" },
+        ],
+      },
+      {
+        text: "SDK",
+        children: [
+          { text: "Gorse Go SDK", icon: "go", link: "https://github.com/gorse-io/gorse/tree/master/client" },
+          { text: "PyGorse - Gorse Python SDK", icon: "python", link: "https://github.com/gorse-io/PyGorse" },
+          { text: "Gorse.js - Gorse TypeScript SDK", icon: "typescript", link: "https://github.com/gorse-io/gorse-js" },
+          { text: "Gorse4J - Gorse Java SDK", icon: "java", link: "https://github.com/gorse-io/gorse4j" },
+          { text: "gorse-rs - Gorse Rust SDK", icon: "rust", link: "https://github.com/gorse-io/gorse-rs" }
+        ],
       },
     ],
-  },
-  {
-    text: "V2 文档",
-    icon: "note",
-    link: "https://vuepress-theme-hope.github.io/v2/zh/",
   },
 ]);
