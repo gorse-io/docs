ebcda6df511ca5ef3c27ea7ee3a1cb5caffa081c
zhenghaoz <zhangzhenghao@hotmail.com>
2023-04-02 15:27:57 +0800

    move sidebar definitions to docs (#80)

--- src/.vuepress/sidebar/en.ts
+++ src/.vuepress/sidebar/en.ts
@@ -1,76 +1,9 @@
 import { sidebar } from "vuepress-theme-hope";
 
-export const enSidebar = sidebar({
-  "/docs/": [
-    {
-      text: 'Quick Start',
-      icon: 'launch',
-      link: '/docs/quick-start',
-    },
-    {
-      text: 'Deploy',
-      icon: 'start',
-      collapsible: true,
-      prefix: '/docs/deploy/',
-      children: [
-        'binary',
-        'docker',
-        'kubernetes'
-      ]
-    },
-    {
-      text: 'Configuration',
-      icon: 'config_s',
-      link: '/docs/config'
-    },
-    {
-      text: 'Concepts',
-      icon: 'idea',
-      collapsible: true,
-      prefix: '/docs/concepts/',
-      children: [
-        'data-objects',
-        'algorithms',
-        'how-it-works',
-        "evaluation"
-      ]
-    },
-    {
-      text: 'API',
-      icon: 'api',
-      collapsible: true,
-      prefix: '/docs/api/',
-      children: [
-        'restful-api',
-        'go-sdk',
-        'python-sdk',
-        'typescript-sdk',
-        'java-sdk',
-        'rust-sdk',
-        'php-sdk',
-        'ruby-sdk',
-        'dotnet-sdk'
-      ]
-    },
-    {
-      text: 'Gorse Dashboard',
-      icon: 'dashboard',
-      link: '/docs/gorse-dashboard'
-    },
-    {
-      text: 'Benchmark',
-      icon: 'dashboard-fill',
-      link: '/docs/benchmark/'
-    },
-    {
-      text: 'Contribution Guide',
-      icon: 'pullrequest',
-      link: '/docs/contribution-guide/'
-    },
-    {
-      text: 'FAQ',
-      icon: 'faq',
-      link: '/docs/faq/'
-    }
-  ],
+var sidebars = {}
+
+import('../../docs/sidebar').then((e) => {
+  sidebars['/docs/'] = e.sidebar;
 });
+
+export const enSidebar = sidebar(sidebars);
--- src/.vuepress/sidebar/zh.ts
+++ src/.vuepress/sidebar/zh.ts
@@ -1,76 +1,9 @@
 import { sidebar } from "vuepress-theme-hope";
 
-export const zhSidebar = sidebar({
-  "/zh/docs/": [
-    {
-      text: '快速上手',
-      icon: 'launch',
-      link: '/zh/docs/quick-start',
-    },
-    {
-      text: '部署',
-      icon: 'start',
-      collapsible: true,
-      prefix: '/zh/docs/deploy/',
-      children: [
-        'binary',
-        'docker',
-        'kubernetes'
-      ]
-    },
-    {
-      text: '配置项',
-      icon: 'config_s',
-      link: '/zh/docs/config'
-    },
-    {
-      text: '概念详解',
-      icon: 'idea',
-      collapsible: true,
-      prefix: '/zh/docs/concepts/',
-      children: [
-        'data-objects',
-        'algorithms',
-        'how-it-works',
-        "evaluation"
-      ]
-    },
-    {
-      text: '接口文档',
-      icon: 'api',
-      prefix: '/zh/docs/api/',
-      collapsible: true,
-      children: [
-        'restful-api',
-        'go-sdk',
-        'python-sdk',
-        'typescript-sdk',
-        'java-sdk',
-        'rust-sdk',
-        'php-sdk',
-        'ruby-sdk',
-        'dotnet-sdk'
-      ]
-    },
-    {
-      text: 'Gorse 控制台',
-      icon: 'dashboard',
-      link: '/zh/docs/gorse-dashboard'
-    },
-    {
-      text: '性能对比',
-      icon: 'dashboard-fill',
-      link: '/zh/docs/benchmark/'
-    },
-    {
-      text: '贡献指南',
-      icon: 'pullrequest',
-      link: '/zh/docs/contribution-guide/'
-    },
-    {
-      text: '常见问题',
-      icon: 'faq',
-      link: '/zh/docs/faq/'
-    }
-  ],
+var sidebars = {}
+
+import('../../zh/docs/sidebar').then((e) => {
+  sidebars['/zh/docs/'] = e.sidebar;
 });
+
+export const zhSidebar = sidebar(sidebars);
