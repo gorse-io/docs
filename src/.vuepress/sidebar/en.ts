34f0e750d3952fe4db171d2a1a486c47d39805da
zhenghaoz <zhangzhenghao@hotmail.com>
2023-04-02 12:21:49 +0800

    split sidebar to docs

--- src/.vuepress/sidebar/en.ts
+++ src/.vuepress/sidebar/en.ts
@@ -1,76 +1,9 @@
 import { sidebar } from "vuepress-theme-hope";
 
-export const enSidebar = sidebar({
-  "/docs/": [
-    {
-      text: 'Quick Start',
-      icon: 'launch',
-      link: 'quick-start',
-    },
-    {
-      text: 'Deploy',
-      icon: 'start',
-      collapsible: true,
-      prefix: 'deploy/',
-      children: [
-        'binary',
-        'docker',
-        'kubernetes'
-      ]
-    },
-    {
-      text: 'Configuration',
-      icon: 'config_s',
-      link: 'config'
-    },
-    {
-      text: 'Concepts',
-      icon: 'idea',
-      collapsible: true,
-      prefix: 'concepts/',
-      children: [
-        'data-objects',
-        'algorithms',
-        'how-it-works',
-        "evaluation"
-      ]
-    },
-    {
-      text: 'API',
-      icon: 'api',
-      collapsible: true,
-      prefix: 'api/',
-      children: [
-        'restful-api',
-        'go-sdk',
-        'python-sdk',
-        'typescript-sdk',
-        'java-sdk',
-        'rust-sdk',
-        'php-sdk',
-        'ruby-sdk',
-        'dotnet-sdk'
-      ]
-    },
-    {
-      text: 'Gorse Dashboard',
-      icon: 'dashboard',
-      link: 'gorse-dashboard'
-    },
-    {
-      text: 'Benchmark',
-      icon: 'dashboard-fill',
-      link: 'benchmark/'
-    },
-    {
-      text: 'Contribution Guide',
-      icon: 'pullrequest',
-      link: 'contribution-guide/'
-    },
-    {
-      text: 'FAQ',
-      icon: 'faq',
-      link: 'faq/'
-    }
-  ],
+var sidebars = {}
+
+import('../../docs/sidebar').then((e) => {
+  sidebars['/docs/'] = e.sidebar;
 });
+
+export const enSidebar = sidebar(sidebars);
