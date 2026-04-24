1c3f1d7dd7f4d59e8094c25705dec364f1af31eb
zhenghaoz <zhangzhenghao@hotmail.com>
2026-04-24 13:47:26 +0800

    feat: add WeChat popup menu instead of image link

--- src/.vuepress/components/WeChatLink.ts
+++ src/.vuepress/components/WeChatLink.ts
@@ -1,20 +1,93 @@
-import { type FunctionalComponent, h } from "vue";
+import { defineComponent, h, ref } from "vue";
 
-const TwitterLink: FunctionalComponent = () =>
-  h(
-    "div",
-    { class: "vp-nav-item vp-action" },
-    h("a", {
-      class: "vp-action-link",
-      href: "https://gorse.io/weixin.jpg",
-      target: "_blank",
-      rel: "noopener noreferrer",
-      "aria-label": "wechat",
-      innerHTML:
-        '<i class="iconfont icon-wechat-fill"></i>',
-    })
-  );
+export default defineComponent({
+  name: "WeChatLink",
+  setup() {
+    const showPopup = ref(false);
 
-TwitterLink.displayName = "DiscordLink";
+    const togglePopup = () => {
+      showPopup.value = !showPopup.value;
+    };
 
-export default TwitterLink;
+    const closePopup = () => {
+      showPopup.value = false;
+    };
+
+    return () =>
+      h(
+        "div",
+        {
+          class: "vp-nav-item vp-action wechat-container",
+          style: { position: "relative" },
+        },
+        [
+          h("a", {
+            class: "vp-action-link",
+            href: "#",
+            "aria-label": "wechat",
+            onClick: (e: Event) => {
+              e.preventDefault();
+              togglePopup();
+            },
+            innerHTML: '<i class="iconfont icon-wechat-fill"></i>',
+          }),
+          showPopup.value
+            ? h(
+                "div",
+                {
+                  class: "wechat-popup",
+                  style: {
+                    position: "absolute",
+                    top: "100%",
+                    right: "0",
+                    marginTop: "8px",
+                    padding: "16px",
+                    backgroundColor: "#fff",
+                    borderRadius: "8px",
+                    boxShadow: "0 4px 12px rgba(0, 0, 0, 0.15)",
+                    zIndex: "100",
+                    textAlign: "center",
+                  },
+                },
+                [
+                  h("img", {
+                    src: "https://gorse.io/weixin.jpg",
+                    alt: "WeChat QR Code",
+                    style: {
+                      width: "150px",
+                      height: "150px",
+                      display: "block",
+                    },
+                  }),
+                  h(
+                    "div",
+                    {
+                      style: {
+                        marginTop: "8px",
+                        fontSize: "14px",
+                        color: "#666",
+                      },
+                    },
+                    "扫码关注公众号"
+                  ),
+                ]
+              )
+            : null,
+          showPopup.value
+            ? h("div", {
+                class: "wechat-popup-overlay",
+                style: {
+                  position: "fixed",
+                  top: "0",
+                  left: "0",
+                  right: "0",
+                  bottom: "0",
+                  zIndex: "99",
+                },
+                onClick: closePopup,
+              })
+            : null,
+        ]
+      );
+  },
+});
